3f0689bc328139963970404c3ec936a773817c5f
andrewroddy92 <andrewroddy92@yahoo.com>
2021-11-23 01:25:27 -0500

    enabling heroku deploy

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 const compression = require("compression");
 
-const PORT = 3000;
+//-------------- EXPRESS SERVER ---------------
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+//-------------- LOGGER ---------------
+
 app.use(logger("dev"));
+
+//--------------  REQ/RES handlers ---------------
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+//--------------  MONGO DB ---------------
+
+const databaseUrl = 'budget'
+const collections = ['budget'];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 // routes
 app.use(require("./routes/api.js"));
